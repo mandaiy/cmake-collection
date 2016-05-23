ae78e75dd9cba75fdb0fb583d37b928e6e62920d
Yusaku Mandai <mandai.yusaku@gmail.com>
2016-05-23 18:31:04 +0900

    Add comments for yamlcpp.cmake

--- yamlcpp.cmake
+++ yamlcpp.cmake
@@ -1,7 +1,21 @@
+# following cmake variables are set for using in your cmake files:
+#  ${YAML_CPP_PROJECT_NAME}: project name of yamlcpp
+#                            you can use this for dependency configuration for example
+#  ${YAML_CPP_INCLUDE_DIR}:  header file direcotry of yamlcpp
+#  ${YAML_CPP_BINARY_DIR}:   binary directory of yamlcpp
+#                            the library and test executables are supposed to be stored in
+#
+# To link yamlcpp you need to add line 'link_directories (${YAML_CPP_BINARY_DIR})'
+# To include yamlcpp header files you need to add line
+#   'include_directories(${YAML_CPP_INCLUDE_DIR})'
+
 cmake_minimum_required (VERSION 2.8.8)
 include                (ExternalProject)
 
-ExternalProject_Add (yamlcpp
+set (YAML_CPP_PROJECT_NAME yamlcpp)
+set (YAML_CPP_LIBRARY_NAME yaml-cpp)
+
+ExternalProject_Add (${YAML_CPP_PROJECT_NAME}
     GIT_REPOSITORY  https://github.com/jbeder/yaml-cpp.git
     GIT_TAG         release-0.5.3
     PREFIX          ${CMAKE_CURRENT_BINARY_DIR}/yaml-cpp
@@ -9,8 +23,8 @@ ExternalProject_Add (yamlcpp
     INSTALL_COMMAND ""
 )
 
-ExternalProject_Get_Property (yamlcpp source_dir)
+ExternalProject_Get_Property (${YAML_CPP_PROJECT_NAME} source_dir)
 set (YAML_CPP_INCLUDE_DIR ${source_dir}/include)
 
-ExternalProject_Get_Property (yamlcpp binary_dir)
+ExternalProject_Get_Property (${YAML_CPP_PROJECT_NAME} binary_dir)
 set (YAML_CPP_BINARY_DIR ${binary_dir})
